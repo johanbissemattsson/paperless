c61b58c6b074e57cf9d78a8664fa84c6d4c01a12
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2017-09-26 13:25:01 +0200

    Add Toolbar button for Android

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Platform, ToolbarAndroid  } from 'react-native';
+import { Constants } from 'expo';
 
 import DocumentButton from './DocumentButton';
 
@@ -8,16 +9,33 @@ export default class MainScreen extends React.Component {
     title: 'Main Screen',
   };
 
+  onActionSelected = (position) => {
+    const { navigate } = this.props.navigation;
+    console.log(position);
+    if (position === 0) { // index of 'Settings'
+      navigate('Settings');
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-      <DocumentButton />
+        <DocumentButton />
         <Text>Detta är main screen</Text>
         <Button
           title="Settings"
           onPress={() => navigate('Settings')}
         />
+        {(Platform.OS === 'android') ?
+          <ToolbarAndroid
+            actions={[{title: 'Settings', show: 'never'},{title: 'Send feedback', show: 'never'},{title: 'Help', show: 'never'}]}
+            onActionSelected={this.onActionSelected}
+            style={styles.toolbar}
+          />
+        :
+        null //add iOS fallback here
+        }
       </View>
     );
   }
@@ -26,8 +44,16 @@ export default class MainScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff9900',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  toolbar: {
+    height: 64,
+    width: 64,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    marginTop: Constants.statusBarHeight
+  }
 });
